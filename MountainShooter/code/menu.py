f8249d997014b99d45df61b53fd7cd205fddf165
CarlosMagno <magnokun24@gmail.com>
2025-08-16 19:16:04 -0300

    Creating

--- MountainShooter/code/menu.py
+++ MountainShooter/code/menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -14,15 +14,20 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0) # Criando um Quadrado para receber a imagem
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.wav') # Carregamento de Sons
         pygame.mixer_music.play(-1) # Parametro para loop infinito
         while True:
+            #DESENHAR AS IMAGENS
             self.window.blit(source=self.surf, dest=self.rect)  # Direcionar a imagem para aparecer na tela
             self.menu_text(50, "Mountain", COLOR_ORANGE , ((WIN_WIDTH / 2), 70)) # Texto Titulo do game
             self.menu_text(50, "Shooter", COLOR_ORANGE , ((WIN_WIDTH / 2), 120)) # Texto Titulo do game
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE ,  ((WIN_WIDTH / 2), 200 + 25 * i))
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE ,  ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()  # Atualizando a tela para aparecer a imagem
 
@@ -31,6 +36,21 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # fechar a janela
                     quit()  #finalizar o pygame
+                #MOVIMENTOS DO MENU - TECLADO
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) -1: #Evento de Tecla pra baixo resetar toda vez que chegar no fim
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
+
 
     ## Importando um metodo para carregar uma font como se fosse imagem
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
