5e5ccb54736dd99716a00af7d065b9c72975b9ea
CarlosMagno <magnokun24@gmail.com>
2025-08-20 16:38:36 -0300

    Hit Box Collisions HUD Score

--- MountainShooter/code/level.py
+++ MountainShooter/code/level.py
@@ -9,7 +9,7 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.EntityMediator import EntityMediator
-from code.const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
+from code.const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN
 from code.enemy import Enemy
 from code.entity import Entity
 from code.entityFactory import EntityFactory
@@ -42,6 +42,10 @@ class Level:
                     shoot = ent.shoot()
                     if shoot is not None:
                         self.entity_list.append(shoot)
+                if ent.name == 'Player1':
+                    self.level_text(14, f'Player1 - Health: {ent.health} | Score: {ent.score}', C_GREEN, (10, 25))
+                if ent.name == 'Player2':
+                    self.level_text(14, f'Player2 - Health: {ent.health} | Score: {ent.score}', C_CYAN, (10, 45))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -51,9 +55,9 @@ class Level:
                     self.entity_list.append(EntityFactory.get_entity(choice))
 
             # Imprimindo os textos
-            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
-            self.level_text(14, f'FPS: {clock.get_fps() :.0f}', COLOR_WHITE, (10,WIN_HEIGHT - 35))
-            self.level_text(14, f'Entidades: {len(self.entity_list)}', COLOR_WHITE, (10,WIN_HEIGHT - 20))
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', C_WHITE, (10, 5))
+            self.level_text(14, f'FPS: {clock.get_fps() :.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'Entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
             # Colisões
             EntityMediator.verify_collision(entity_list=self.entity_list)
